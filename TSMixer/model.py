3b3cd6e1d1d77175d5ad1f72d89c6244571f0eca
Kashif Rasul <8100+kashif@users.noreply.github.com>
2023-04-05 16:18:51 +0300

    initial empty TSMixer estimator

--- TSMixer/model.py
+++ TSMixer/model.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 
+
 class MLP_Time(nn.Module):
     """MLP for time embedding.
 
@@ -17,12 +18,14 @@ class MLP_Time(nn.Module):
             nn.BatchNorm1d(ts_length),
             nn.Linear(ts_length, ts_length),
             nn.ReLU(),
-            nn.Dropout(dropout)
+            nn.Dropout(dropout),
         )
 
     def forward(self, x):
-        x_time = self.time_mlp(x.transpose(0,1))
-        return x + x_time.transpose(0,1) # not sure if we need a residual connection here. The paper doesn't mention it.
+        x_time = self.time_mlp(x.transpose(0, 1))
+        return x + x_time.transpose(
+            0, 1
+        )  # not sure if we need a residual connection here. The paper doesn't mention it.
 
 
 class MLP_Feat(nn.Module):
@@ -43,17 +46,17 @@ class MLP_Feat(nn.Module):
             nn.BatchNorm1d(in_channels),
             nn.Linear(in_channels, embed_dim),
             nn.ReLU(),
-            nn.Dropout(dropout)
+            nn.Dropout(dropout),
         )
 
         self.feat_mlp2 = nn.Sequential(
-            nn.Linear(embed_dim, in_channels),
-            nn.Dropout(dropout)
+            nn.Linear(embed_dim, in_channels), nn.Dropout(dropout)
         )
 
     def forward(self, x):
         u = self.feat_mlp1(x)
         return x + self.feat_mlp2(u)
+
 
 class Mixer_Block(nn.Module):
     def __init__(self, ts_length, in_channels, embed_dim, dropout=0.1):
@@ -77,8 +80,11 @@ class Mixer_Block(nn.Module):
         x = self.mlp_feat(x)
         return x
 
+
 class TS_Mixer(nn.Module):
-    def __init__(self, ts_length, in_channels, embed_dim, num_blocks, fcst_h, dropout=0.1):
+    def __init__(
+        self, ts_length, in_channels, embed_dim, num_blocks, fcst_h, dropout=0.1
+    ):
         """Time Series Mixer.
 
         :argument
@@ -93,12 +99,15 @@ class TS_Mixer(nn.Module):
             - x (tensor): output tensor of shape (fcst_h, in_channels)
         """
         super().__init__()
-        self.mixer_blocks = nn.Sequential(*[
-            Mixer_Block(ts_length, in_channels, embed_dim, dropout) for _ in range(num_blocks)
-        ])
+        self.mixer_blocks = nn.Sequential(
+            *[
+                Mixer_Block(ts_length, in_channels, embed_dim, dropout)
+                for _ in range(num_blocks)
+            ]
+        )
         self.fc = nn.Linear(ts_length, fcst_h)
 
     def forward(self, x):
         x = self.mixer_blocks(x)
-        x = self.fc(x.transpose(0,1))
-        return x.transpose(0,1)
+        x = self.fc(x.transpose(0, 1))
+        return x.transpose(0, 1)
