8034e846d173715175fa583d177d646110042b14
Alexander März <alex.maerz@gmx.net>
2023-04-12 11:25:54 +0200

    Fixed problem with forward pass of auxiliary model

--- TSMixer/model_auxiliary.py
+++ TSMixer/model_auxiliary.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from .model import MLP_Time as MLP_Time_Block
 from .model import MLP_Feat as MLP_Feat_Block
 
+
 class MLP_Feat(nn.Module):
     """MLP on feature domain.
 
@@ -45,37 +46,6 @@ class MLP_Feat(nn.Module):
         return out
 
 
-# class MLP_covariate(nn.Module):
-#     """MLP for X and Z covariates.
-#
-#     :argument
-#         - n_feat (int): number of features
-#         - embed_dim (int): embedding dimension
-#         - dropout (float): dropout rate
-#
-#     :return
-#         - x (tensor): output tensor of shape (batch_size, ts_length, n_feat)
-#     """
-#
-#     def __init__(self, n_feat, fcst_h, embed_dim, dropout=0.1):
-#         super().__init__()
-#         self.cov_mlp1 = nn.Sequential(
-#             nn.BatchNorm1d(fcst_h),
-#             nn.Linear(n_feat, embed_dim),
-#             nn.ReLU(),
-#             nn.Dropout(dropout)
-#         )
-#
-#         self.cov_mlp2 = nn.Sequential(
-#             nn.Linear(embed_dim, n_feat),
-#             nn.Dropout(dropout)
-#         )
-#
-#     def forward(self, x):
-#         x_feat = self.cov_mlp1(x)
-#         return x + self.cov_mlp2(x_feat)
-
-
 class Mixer_Block(nn.Module):
     """Mixer block.
 
@@ -99,17 +69,54 @@ class Mixer_Block(nn.Module):
 
         self.mlp_time = MLP_Time_Block(fcst_h, dropout)
         self.mlp_s = MLP_Feat(n_static_feat, embed_dim, dropout)
-        self.mlp_feat = MLP_Feat_Block(embed_dim*3, embed_dim, dropout)
+        self.mlp_feat = MLP_Feat_Block(n_feat, embed_dim, dropout)
 
         # We apply an additional linear transformation on the output of MLP_Feat_Block. Otherwise, each block increase
         # the output dimension by embed_dim
-        self.projector = nn.Linear(embed_dim*3, embed_dim*2)  # check again if this is necessary.
+        self.projector = nn.Linear(n_feat, embed_dim*2)  # check again if this is necessary.
 
     def forward(self, x, s):
         x = self.mlp_time(x)
         out = self.mlp_feat(torch.cat([x, self.mlp_s(s)], dim=2))
         out = self.projector(out)
         return out
+
+
+class Mixer(nn.Module):
+    """Mixer.
+
+    :argument
+        - n_feat (int): number of input features
+        - n_static_feat (int): number of static features
+        - fcst_h (int): forecast horizon
+        - embed_dim (int): embedding dimension
+        - num_blocks (int): number of mixer blocks
+        - dropout (float): dropout rate, default 0.1
+
+    :return
+        - x (tensor): output tensor of shape (batch_size, fcst_h, embed_dim)
+    """
+    def __init__(self,
+                 n_feat: int,
+                 n_static_feat: int,
+                 fcst_h: int,
+                 embed_dim: int,
+                 num_blocks: int,
+                 dropout: float = 0.1):
+        super(Mixer, self).__init__()
+        self.mixer_blocks = nn.ModuleList([
+            Mixer_Block(n_feat,
+                        n_static_feat,
+                        fcst_h,
+                        embed_dim,
+                        dropout)
+            for _ in range(num_blocks)
+        ])
+
+    def forward(self, x, s):
+        for mixer_block in self.mixer_blocks:
+            x = mixer_block(x, s)
+        return x
 
 
 class TS_Mixer_auxiliary(nn.Module):
@@ -158,17 +165,15 @@ class TS_Mixer_auxiliary(nn.Module):
         self.mlp_z = MLP_Feat(n_feat_sz, embed_dim, dropout)
 
         # Mixer blocks
-        self.mixer_blocks = nn.Sequential(*[
-            Mixer_Block(embed_dim*2,
-                        n_static_feat,
-                        fcst_h,
-                        embed_dim,
-                        dropout)
-            for _ in range(num_blocks)
-        ])
+        self.mixer_blocks = Mixer(embed_dim*3,
+                                  n_static_feat,
+                                  fcst_h,
+                                  embed_dim,
+                                  num_blocks,
+                                  dropout)
 
         # MLP that maps the output of the mixer blocks to the output dimension
-        self.mlp_out = nn.Linear(embed_dim, out_dim)
+        self.mlp_out = nn.Linear(embed_dim*2, out_dim)
 
         # Layer normalization
         self.layer_norm = nn.LayerNorm(out_dim)
@@ -183,6 +188,6 @@ class TS_Mixer_auxiliary(nn.Module):
         x_prime = self.mlp_x(torch.cat([x, self.mlp_sx(s)], dim=2))
         z_prime = self.mlp_z(torch.cat([z, self.mlp_sz(s)], dim=2))
         y_prime = torch.cat([x_prime, z_prime], dim=2)
-        y_prime = self.mixer_blocks(y_prime, s)
-        out = self.layer_norm(self.mlp_out(y_prime))
+        y_prime_block = self.mixer_blocks(y_prime, s)
+        out = self.layer_norm(self.mlp_out(y_prime_block))
         return out
